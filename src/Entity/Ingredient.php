7fc67ea0653c3338a97e9219dc757a3d5483980d
Dekhil Omran <dekhil22omran@gmail.com>
2023-06-10 09:57:54 +0100

    Home controller

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(min: 2, max: 50)]
+    #[Assert\NotNull(message: "can not be null :/")]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    #[Assert\Positive()]
+    #[Assert\LessThan(200)]
+    #[Assert\NotNull(message: "can not be null :/")]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull(message: "can not be null :/")]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
